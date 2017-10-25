d76ce7e982300d06b07b81c3484a82c2d324eab4
Theo Breuer-Weil <theobreuerweil@gmail.com>
2017-10-25 12:45:45 +0100

    expands docking_station spec to include docking; refactors docking_station spec

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -2,12 +2,39 @@ require "classes"
 
 describe DockingStation do
   
-  # docking station tests
-  it { expect(DockingStation.new).to respond_to(:release_bike) }
-  it { expect(DockingStation.new.release_bike).to be_a Bike }
+  subject(:docking_station) { described_class.new }
 
-  # bike test
-  subject(:bike) { described_class.new.release_bike }
-  it { expect(bike).to be_working }
+  context 'when created' do
+
+    it { expect(docking_station.bike).to be_nil }
+
+  end
+
+  context 'docks bikes' do
+
+    it { expect(docking_station).to respond_to(:dock) }
+
+    it 'accepts bikes for docking' do
+      bike = Bike.new
+      docking_station.dock(bike)
+      expect(docking_station.bike).to eq bike
+    end
+
+    it "doesn't accept non-bikes for docking" do
+      expect { docking_station.dock(4) }.to raise_error(ArgumentError)
+    end
+
+  end
+
+  context 'releases bikes' do
+
+    subject(:bike) { described_class.new.release_bike }
+
+    it { expect(docking_station).to respond_to(:release_bike) }
+    it { expect(docking_station.release_bike).to be_a Bike }
+    it { expect(bike).to be_working }
+
+  end
+
 
 end
