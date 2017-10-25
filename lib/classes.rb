03e2a094cef6e8f903077d798c15a1d8cff97c96
cristhiandas <cristhiandf94@gmail.com>
2017-10-25 15:06:52 +0100

    Updated docking_station.rb to get more than 1 bike and tells if there are no bikes at the docking station

--- lib/classes.rb
+++ lib/classes.rb
@@ -1,23 +1,35 @@
 class DockingStation
 
-  attr_reader :bike
+  attr_reader :bikes
 
   def initialize
-    @bike = nil
+    @bikes = []
   end
 
   def dock(bike)
     if !bike.is_a? Bike
       raise ArgumentError, "expecting a bike, not #{bike.class}"
     end
-    @bike = bike
+    @bikes << bike
+    self
+  end
+
+  def do_release
+    @bikes.pop
   end
 
   def release_bike
-    Bike.new
+    bike = do_release
+    return bike unless bike.nil?
+    raise RuntimeError, "no bikes available"
+  end
+
+def has_bike?
+  !@bikes.empty?
   end
 
 end
+
 
 class Bike
   def working?
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,40 +1,56 @@
 require "classes"
 
 describe DockingStation do
-  
+
   subject(:docking_station) { described_class.new }
+  subject(:bike) { Bike.new }
 
-  context 'when created' do
-
-    it { expect(docking_station.bike).to be_nil }
-
+  describe 'when created' do
+    it { expect(docking_station.bikes).to be_a Array }
+    it { expect(docking_station.bikes).to be_empty }
   end
 
-  context 'docks bikes' do
-
+  describe 'docks bikes' do
     it { expect(docking_station).to respond_to(:dock) }
 
     it 'accepts bikes for docking' do
-      bike = Bike.new
       docking_station.dock(bike)
-      expect(docking_station.bike).to eq bike
+      expect(docking_station.bikes[-1]).to eq bike
     end
 
     it "doesn't accept non-bikes for docking" do
       expect { docking_station.dock(4) }.to raise_error(ArgumentError)
     end
-
   end
 
-  context 'releases bikes' do
+  describe 'releases bikes' do
 
-    subject(:bike) { described_class.new.release_bike }
+    context "supports methods" do
+      it { expect(docking_station).to respond_to(:has_bike?) }
+      it { expect(docking_station).to respond_to(:release_bike) }
+    end
 
-    it { expect(docking_station).to respond_to(:release_bike) }
-    it { expect(docking_station.release_bike).to be_a Bike }
-    it { expect(bike).to be_working }
+    context "with no bikes" do
 
+      it "raises " do
+        expect { docking_station.release_bike }.to raise_error(RuntimeError)
+      end
+
+      it { expect(docking_station).to_not have_bike }
+    end
+
+    context "with bikes" do
+
+      it "It is a Bike" do
+        expect(docking_station.dock(Bike.new).release_bike).to be_a Bike
+      end
+
+      it "consumes one bike" do
+        docking_station.dock(Bike.new)
+        docking_station.release_bike
+        expect(docking_station).to_not have_bike
+      end
+
+    end
   end
-
-
 end
