477a6506712ea1a7a52fbfac519b1c64121d8fd2
Theo Breuer-Weil <theobreuerweil@gmail.com>
2017-10-25 12:46:02 +0100

    adds docking to docking_station

--- lib/classes.rb
+++ lib/classes.rb
@@ -1,5 +1,18 @@
 class DockingStation
 
+  attr_reader :bike
+
+  def initialize
+    @bike = nil
+  end
+
+  def dock(bike)
+    if !bike.is_a? Bike
+      raise ArgumentError, "expecting a bike, not #{bike.class}"
+    end
+    @bike = bike
+  end
+
   def release_bike
     Bike.new
   end
